11e42f98ec3bd665c886c6e0e63723b63944da0f
rndall <96964503+rndall@users.noreply.github.com>
2025-01-15 23:19:08 +0800

    Add GameController object

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function Gameboard() {
 function Player(playerName, symbol) {
 	return { playerName, symbol };
 }
+
+function GameController(
+	playerOneName = "Player One",
+	playerTwoName = "Player Two",
+) {
+	const board = Gameboard();
+
+	const players = [Player(playerOneName, "X"), Player(playerTwoName, "O")];
+
+	let activePlayer = players[0];
+	const switchPlayerTurn = () => {
+		activePlayer = activePlayer === players[0] ? players[1] : players[0];
+	};
+}
